75cee8e2a8fa035397811e07cf95226b08a51ed5
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-17 15:01:13 -0800

    Update script.js

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,1 +1,51 @@
+const box = document.getElementById('hovbin');
 
+
+box.addEventListener('mouseover', function handleMouseOver() {
+  box.style.color = 'red';
+});
+
+
+box.addEventListener('mouseout', function handleMouseOut() {
+  box.style.color = 'black';
+}); 
+
+
+
+const box1 = document.getElementById('hovlon');
+
+
+box1.addEventListener('mouseover', function handleMouseOver() {
+  box1.style.color = 'red';
+});
+
+
+box1.addEventListener('mouseout', function handleMouseOut() {
+  box1.style.color = 'white';
+}); 
+
+
+
+const box2 = document.getElementById('hovkin');
+
+
+box2.addEventListener('mouseover', function handleMouseOver() {
+  box2.style.color = 'red';
+});
+
+
+box2.addEventListener('mouseout', function handleMouseOut() {
+  box2.style.color = 'black';
+}); 
+
+const box3 = document.getElementById('hovhom');
+
+
+box3.addEventListener('mouseover', function handleMouseOver() {
+  box3.style.color = 'red';
+});
+
+
+box3.addEventListener('mouseout', function handleMouseOut() {
+  box3.style.color = 'white';
+}); 
