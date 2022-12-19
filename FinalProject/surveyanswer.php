29c5c4a52c045ee6836384f3dffeb89a93dfd254
Brandon Le <111783780+CenaMonster@users.noreply.github.com>
2022-12-19 09:36:01 -0800

    Create surveyanswer.php

---
+++ FinalProject/surveyanswer.php
@@ -0,0 +1,11 @@
+<?php
+$FavOpening = $_POST['FavOpening'];
+echo "<h2> We will try to add " . $FavOpening . " to this website. </h2>";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style.css"> <!-- external style -->
+</head>
+</html>
