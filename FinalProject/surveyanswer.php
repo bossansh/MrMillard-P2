1daaac992303b15ad27c462a8aa43fb9855bd265
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-20 15:46:29 -0800

    Update surveyanswer.php

--- FinalProject/surveyanswer.php
+++ FinalProject/surveyanswer.php
@@ -1,30 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    
+    <?php 
+    $server = "localhost"; //Please edit the username and password to what suits your computer.
+    $username = "root";
+    $password = "";
+    $dbname = "Chess";
 
-<?php 
-$server = "localhost"; //Please edit the username and password to what suits your computer.
-$username = "root";
-$password = "";
-$dbname = "Chess";
+    $conn = mysqli_connect($server, $username, $password, $dbname);
 
-$conn = mysqli_connect($server, $username, $password, $dbname);
-
-if(isset($_POST['submit'])){
-  if(!empty($_POST['FavOpening'])){
-    $FavOpening = $_POST['FavOpening'];
-    $query = "insert into favorite_chess_opening?_(Chess_answers) values('$FavOpening')";
-    $run = mysqli_query($conn, $query) or die(mysqli_error());
-    if($run){
-      echo "<h2>Hi</h2>";
-      $sql = "SELECT * FROM Chess WHERE Chess_answers = '$FavOpening'";
-      echo "<h2>We will try to add" . $sql . "to this website!</h2>";
+    if(isset($_POST['submit'])){
+      if(!empty($_POST['FavOpening'])){
+        $FavOpening = $_POST['FavOpening'];
+        $query = "insert into favorite_chess_opening?_(Chess_answers) values('$FavOpening')";
+        $run = mysqli_query($conn, $query) or die(mysqli_error());
+        if($run){
+          echo "<h2>Hi</h2>";
+          $sql = "SELECT * FROM Chess WHERE Chess_answers = '$FavOpening'";
+          echo "<h2>We will try to add" . $sql . "to this website!</h2>";
+          }
+        else{
+          echo "<h2>Form not submitted</h2>";
+        }
       }
-    else{
-      echo "<h2>Form not submitted</h2>";
+      else{
+        echo "<h2>All fields required</h2>";
+      }
+
     }
-  }
-  else{
-    echo "<h2>All fields required</h2>";
-  }
-
-}
-?>
-
+  ?>
+  </body>
+</html>
