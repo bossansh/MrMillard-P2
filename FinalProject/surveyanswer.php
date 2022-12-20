4389c8ca761b03d740e29965eec0ffc727324198
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-20 14:50:35 -0800

    Update surveyanswer.php

--- FinalProject/surveyanswer.php
+++ FinalProject/surveyanswer.php
@@ -9,7 +9,7 @@
 
     if(isset($_POST['submit'])){
       if(!empty($_POST['FavOpening'])){
-        $FavOpening = $_POST['$FavOpening'];
+        $FavOpening = $_POST['FavOpening'];
         $query = "insert into favorite chess opening?(Chess_answers) values('$FavOpening')";
         $run = mysqli_query($conn, $query) or die(mysqli_error());
         if($run){
