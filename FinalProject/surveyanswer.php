1635bd51ddb926738031100f627d3a688f1bba77
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-19 20:08:26 -0800

    Update surveyanswer.php

--- FinalProject/surveyanswer.php
+++ FinalProject/surveyanswer.php
@@ -6,4 +6,21 @@ $dbname = "Chess";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
-if(isset($_POST
+if(isset($_POST['submit'])){
+  if(!empty($_POST['FavOpening'])){
+    $FavOpening = $_POST['$FavOpening'];
+    $query = "insert into favorite chess opening?(Chess_answers) values('$FavOpening')";
+    $run = mysqli_query($conn, $query) or die(mysqli_error());
+    if($run){
+      echo "form submission successful";
+      }
+    else{
+      echo "Form not submitted";
+    }
+  }
+  else{
+    echo "All fields required";
+  }
+  
+}
+?>
