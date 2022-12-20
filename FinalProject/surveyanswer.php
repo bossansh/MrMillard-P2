5c981586c6ac5820ccb2d60af6c31829bc88c808
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-20 15:04:54 -0800

    Update surveyanswer.php

--- FinalProject/surveyanswer.php
+++ FinalProject/surveyanswer.php
@@ -1,28 +1,33 @@
 
-    <?php 
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Chess";
+<?php 
+/*CREATE DATABASE Chess;
+  CREATE TABLE favorite_chess_opening? (
+    ChessID int,
+    Chess_answers varchar(255),
+);*/
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Chess";
 
-    $conn = mysqli_connect($server, $username, $password, $dbname);
+$conn = mysqli_connect($server, $username, $password, $dbname);
 
-    if(isset($_POST['submit'])){
-      if(!empty($_POST['FavOpening'])){
-        $FavOpening = $_POST['FavOpening'];
-        $query = "insert into favorite chess opening?(Chess_answers) values('$FavOpening')";
-        $run = mysqli_query($conn, $query) or die(mysqli_error());
-        if($run){
-          echo "<h2>We will try to add" . $FavOpening . "to this website!</h2>";
-          }
-        else{
-          echo "<h2>Form not submitted</h2>";
-        }
+if(isset($_POST['submit'])){
+  if(!empty($_POST['FavOpening'])){
+    $FavOpening = $_POST['FavOpening'];
+    $query = "insert into favorite chess opening?(Chess_answers) values('$FavOpening')";
+    $run = mysqli_query($conn, $query) or die(mysqli_error());
+    if($run){
+      echo "<h2>We will try to add" . $FavOpening . "to this website!</h2>";
       }
-      else{
-        echo "<h2>All fields required</h2>";
-      }
-
+    else{
+      echo "<h2>Form not submitted</h2>";
     }
-    ?>
+  }
+  else{
+    echo "<h2>All fields required</h2>";
+  }
+
+}
+?>
 
