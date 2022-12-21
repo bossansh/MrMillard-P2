e82727bf65fb518a8cc1941f113f127bb6ee57ae
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-20 16:02:47 -0800

    Update survey.php

--- FinalProject/survey.php
+++ FinalProject/survey.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Survey</title>
+    <title>Survey</title> <!--Title of page and referring to phpcss.css for the css -->
     <link rel="stylesheet" href="phpcss.css"> <!-- external style -->
     <style>
       html {
         background-image: url('IMAGES/kingdown.jpeg');
-        background-size: cover;
+        background-size: cover; <!-- adds background image to the page -->
         min-height: 100%;
         background-repeat: no-repeat;
         background-position: center center;
@@ -16,12 +16,12 @@
   </head>
   <body>
     <form action="surveyanswer.php" method="post">
-       <h2>What is your favorite chess opening?</h2>
+       <h2>What is your favorite chess opening?</h2> <!-- makes survey with the question of the favorite opening, and when submitted, goes to surveyanswer.php -->
        <label>Favorite Opening</label>
        <input type="text" name="FavOpening" placeholder="Opening name"><br>
        <button type="submit" name = "submit">Submit</button>
     </form>
-    <li style=""><a href="index.html" style="background-color: #1a1a1a;">Back</a></li>
+    <li style=""><a href="index.html" style="background-color: #1a1a1a;">Back</a></li> <!-- goes back to home if the user wishes to do so -->
   </body>
 </html>
 
