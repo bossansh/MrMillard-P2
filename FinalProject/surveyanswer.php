2e208369c753df120f50438d3205d3e5d1ea5101
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2022-12-25 09:37:06 -0800

    Update surveyanswer.php

--- FinalProject/surveyanswer.php
+++ FinalProject/surveyanswer.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="phpcss.css"> <!-- external style -->
     <style>
       html {
-        background-image: url('IMAGES/drawnpieces.jpeg');
+        background-image: url('IMAGES/leader-success-chess-pieces.jpeg');
         background-size: cover; /* adds background image to the page */
         min-height: 100%;
         background-repeat: no-repeat;
@@ -14,33 +14,57 @@
     </style>
   </head>
   <body>
-    <li style=""><a href="index.html" style="background-color: #1a1a1a;">Home</a></li> <!-- button to go back to home page -->
+    <a href="index.html" style="background-color: #1a1a1a;">Home</a><!-- button to go back to home page -->
     <?php 
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+      
     $server = "localhost"; 
     $username = "root";
     $password = "";
     $dbname = "Chess";
 
     $conn = mysqli_connect($server, $username, $password, $dbname); //connects to sql file based on these constraints
+    
 
     if(isset($_POST['submit'])){ //checks if survey was submitted
-      if(!empty($_POST['FavOpening'])){
+      if(!empty($_POST['FavOpening'] && !empty($_POST['name']))){
         $FavOpening = $_POST['FavOpening'];
-        $query = "insert into favorite_chess_opening?_(Chess_answers) values('$FavOpening')"; //inserts results of survey into the sql in Chess_answers column
-        $run = mysqli_query($conn, $query) or die(mysqli_error()); //runs the query if possible, otherwise yields an error
-        if($run){
-          $sql = "SELECT * FROM Chess WHERE Chess_answers = '$FavOpening'"; //takes the answer that is equal to the survey input...
-          echo "<h2>We will try to add" . $sql . "to this website!</h2>"; //...and then places into here for this sentence
-          }
-        else{
-          echo "<h2>Form not submitted</h2>";
+        $name = $_POST['name'];
+        $sql = "SELECT count(*) as cnt FROM `favorite chess opening?` WHERE Chess_answers = '$FavOpening'"; //takes the number of values in database that is equal to $FavOpening
+        $result = mysqli_query($conn, $sql);
+        while ($row = $result->fetch_assoc()) {
+                $column = $row["cnt"];
         }
+        if($column == 0) //if there are no values that are equal to users input then runs this code
+        {
+            $query = "insert into `favorite chess opening?` (Name, Chess_answers) values('$name', '$FavOpening')"; //inserts results of survey into the sql in Chess_answers column
+            $run = mysqli_query($conn, $query) or die(mysqli_error()); //runs the query if possible, otherwise yields an error
+            if($run)
+            {
+                echo '<span style="color:#FFF;text-align:center;"><h2>We will try to add ' . $FavOpening . ' to our website.</h2></span>'; //message for user
+            }
+            else
+            {
+                echo "<h2>Unable to enter value.</h2>";
+            }
+        }
+       else{
+            echo '<span style="color:#FFF;text-align:center;"><h2>We are already working on adding ' . $FavOpening . ' to our website.</h2></span>'; //if the value is in database, runs this code
+       }
+      
+        
+          
+        
       }
       else{
-        echo "<h2>All fields required</h2>"; //if the input bar is empty, this error message will pop up
+        echo '<span style="color:#FFF;text-align:center;"><h2>All fields required</h2></span>'; //if the input bar is empty, this error message will pop up
       }
 
     }
+      
     ?>
   </body>
 </html>
