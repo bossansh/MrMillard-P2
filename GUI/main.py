b4a5e9f700415c3d1ae6ed4882d937725f19cb75
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2023-01-24 18:48:41 -0800

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -1,3 +1,11 @@
+#############################################################################
+# Name: Anshul Prabu #221                                                   #
+# Project: First GUI 													    #
+#                                                                           #
+# Description: This GUI has a button which allows a user to select a text   #
+# from their files, and this text will pop-up below							#
+#############################################################################
+
 import tkinter as tk
 import PyPDF2 
 from PIL import * #Image, ImageTk
@@ -9,7 +17,7 @@ canvas = tk.Canvas(main, width=600, height=300)
 canvas.grid(columnspan=3, rowspan=3) #splits canvas into three identical columns for precision
 
 #logo
-logo = Image.open('logo.png')
+logo = Image.open('logo.png') #takes logo image and places it in window
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
@@ -17,9 +25,9 @@ logo_label.grid(column=1, row=0)
 
 #instructions
 instructions = tk.Label(main, text="Select a PDF file on your computer to extract its text", font="Papyrus")
-instructions.grid(columnspan = 3, column=0, row=1)
+instructions.grid(columnspan = 3, column=0, row=1) #places instructions in certain part of window
 
-def open_file():
+def open_file(): #opens user's files, and asks to select one to be placed into window
     browse_text.set("loading... ")
     file = askopenfile(parent=main, mode="rb", title="choose a file", filetype=[("Pdf file", "*.pdf")])
     if file:
@@ -31,12 +39,12 @@ def open_file():
         text_box.insert(1.0, page_content)
         text_box.tag_configure("center", justify="center")
         text_box.tag_add("center", 1.0, "end")
-        text_box.grid(colun=1, row=3)
+        text_box.grid(column=1, row=3)
 
         browse_text.set("Browse")
 
 #browse button
-browse_text = tk.StringVar()
+browse_text = tk.StringVar() #runs open_file on button press.
 browse_btn = tk.Button(main, textvariable=browse_text, command=lambda:open_file(), font ="Papyrus", bg="#69420a", fg="white", height=2, width=15)
 browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
