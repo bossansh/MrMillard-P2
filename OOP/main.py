b4de0994d6e9b49e4523d20899d9768301b9f02a
Anshul Prabu <68819278+bossansh@users.noreply.github.com>
2023-03-01 10:04:41 -0800

    Update main.py

--- OOP/main.py
+++ OOP/main.py
@@ -1,25 +1,33 @@
-#############################################################################
+##############################################################################
 # Name: Anshul Prabu #221                                                   #
 # Project: OOP Practice													    #
 #                                                                           #
 # Description: Creates vehicle class, and gives it two attributes. Makes a  #
-# bus class and copies its attributes				                        #                   #
+# bus class and copies its attributes. Prints out attributes.				#                        #                   #
 #############################################################################
 
 
 class Vehicle(): #step 1 Creates class
-    def __init__(self, mileage, maxspeed): #step 2 creates attributes for vehicle
-        self.milesper = mileage
+    def __init__(self, color, Vehiclename, mileage, maxspeed): #step 2 creates attributes for vehicle
+        self.milesper = mileage 
         self.Max = maxspeed
-        pass
+        self.name = Vehiclename
+        self.Color = color
+    def update(self): #prints variables when update runs
+        print("Color: ", self.Color, "Vehicle Name: ", self.name, "Speed: ", self.Max, "Mileage: ", self.milesper)
+    def seating_capacity(self, capacity): 
+        print("The seating capacity of a ", self.name, "is ", capacity, "passengers")#prints capacity
+        #on call
+    def fare(self, capacity): 
+        return capacity * 100
         
 class Bus(Vehicle): #step 3 Creates bus class and gives it Vehicle's attributes
-    def __init__(self, Vehicle, Vehiclename):
-        super().__init__()
-        self.name = Vehiclename
-    def update(self):
-        print("Vehicle Name: ", self.name, "Speed: ", self.Max, "Mileage: ", self.milesper)
-
-schoolVehicle = Vehicle(12, 180)
-schoolBus = Bus(schoolVehicle, "School Lambo")
-Bus.update(schoolBus)
+    pass
+class Car(Vehicle): #Creates car class and gives vehicle's attributes
+    pass
+capacity = 50
+schoolBus = Bus("white", "School Lambo", 12, 180) #step 4 allows user to inputparameters
+Audi = Car("white", "School Lambo", 18, 240) #makes the car parameters
+Vehicle.update(schoolBus) #runs update and prints them
+Vehicle.update(Audi) #step 6 runs update and prints car stats
+Vehicle.seating_capacity(schoolBus, 50) #step 5
